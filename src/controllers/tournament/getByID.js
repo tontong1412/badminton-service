066989a1794c520eb9609a038ac946a2e920218e
Tontong <tontongfordev@gmail.com>
2022-04-06 14:38:49 +0700

    fix populate too slow

--- src/controllers/tournament/getByID.js
+++ src/controllers/tournament/getByID.js
@@ -4,7 +4,6 @@ const TournamentModel = tournament.model
 
 const getByIDTournament = async (req, res) => {
   const { id } = req.params
-
   let getByIDResponse
   try {
     getByIDResponse = await TournamentModel.findById(id)
@@ -20,24 +19,27 @@ const getByIDTournament = async (req, res) => {
         }
       }
     }).execPopulate()
-    let populateGroup = []
-    const populateEvent = await Promise.all(getByIDResponse.events.map(async (event, i) => {
-      event.order?.group?.forEach((group, j) => populateGroup.push(`order.group.${j}`))
-      return `events.${i}`
-    }))
 
-    await getByIDResponse.populate({
-      path: ` events events.teams ${populateEvent?.join(' ')}`,
-      populate: {
-        path: `team players teams.contact teams.team ${populateGroup?.join(' ')} order.singleElim`,
-        options: { retainNullValues: true },
-        populate: {
-          path: 'team players',
-          options: { retainNullValues: true }
+    // for populate order ===> not use due to slow performance
 
-        }
-      }
-    }).execPopulate()
+    // let populateGroup = []
+    // const populateEvent = await Promise.all(getByIDResponse.events.map(async (event, i) => {
+    //   event.order?.group?.forEach((group, j) => populateGroup.push(`order.group.${j}`))
+    //   return `events.${i}`
+    // }))
+    // await getByIDResponse.populate({
+    //   path: `${populateEvent?.join(' ')}`,
+    //   populate: {
+    //     path: `${populateGroup?.join(' ')} order.singleElim`,
+    //     options: { retainNullValues: true },
+    //     populate: {
+    //       path: 'team players',
+    //       options: { retainNullValues: true }
+
+    //     }
+    //   }
+    // }).execPopulate()
+
   } catch (error) {
     console.error('Error: Get by ID tournament had failed')
     throw error
