c0b049a62b184cc8bfaa7603561e39965a6e957a
Tontong <tontongfordev@gmail.com>
2022-05-01 19:06:30 +0700

    badbear

--- src/script/register.js
+++ src/script/register.js
@@ -10,7 +10,7 @@ import '../libs/mongo/getMongoConnect'
 // Create a document object using the ID of the spreadsheet - obtained from its URL.
 const script = async () => {
 
-  const doc = new GoogleSpreadsheet('13lUwZ5bxzTrQBozC8Az6oXZ5DtaCfqCNz5eA0xL9sCw')
+  const doc = new GoogleSpreadsheet('1Rn85rWWFnP6bz2uDNwdFhYAYrT9Z95S2HETGtkkhVzI')
   // getMongoConnect()
   const mapGenderDetail = {
     ชาย: {
@@ -24,46 +24,35 @@ const script = async () => {
   }
   await doc.useServiceAccountAuth(creds);
 
+  const MAP_EVENT_ID = {
+    'จำกัดมือ N/S': '626d2557e06bba034e2a2aed',
+    'จำกัดมือ P-/P': '626d2592e06bba034e2a2aee',
+    'จำกัดมือ C': '626d25afe06bba034e2a2aef'
+  }
+
   await doc.loadInfo(); // loads document properties and worksheets
-  const sheet = doc.sheetsByIndex[19]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
   console.log(sheet.title);
   console.log(sheet.rowCount);
   const rows = await sheet.getRows()
   rows.forEach((row, i) => {
     const params = {
-      // eventID: '624b364df01eeb1a9aaaed25', //N
-      // eventID: '624b367bf01eeb1a9aaaed26', //S
-      // eventID: '624b36a7f01eeb1a9aaaed27', //P-
-      // eventID: '624b36c3f01eeb1a9aaaed28', //P
-      // eventID: '624b36ddf01eeb1a9aaaed29', //P+
-      // eventID: '624becd9a5775ea1215a18a1', //BS 8
-      // eventID: '624bed54a5775ea1215a18a3', //BS 10
-      // eventID: '624bed88a5775ea1215a18a5', //BS 12
-      // eventID: '624bee70a5775ea1215a18a7', //BS 14
-      // eventID: '624bee96a5775ea1215a18a9', //BS 16
-      // eventID: '624bed11a5775ea1215a18a2', //GS 8
-      // eventID: '624bed6aa5775ea1215a18a4', //GS 10
-      // eventID: '624beddea5775ea1215a18a6', //GS 12
-      // eventID: '624bee83a5775ea1215a18a8', //GS 14
-      // eventID: '624beeaba5775ea1215a18aa', //GS 16
-      // eventID: '624beedca5775ea1215a18ab', //xd 8
-      // eventID: '624bef06a5775ea1215a18ac', //xd 10
-      // eventID: '624bef18a5775ea1215a18ad', //xd 12
-      // eventID: '624bef2da5775ea1215a18ae', //xd 14
-      eventID: '624bef44a5775ea1215a18af', //xd 16
+      eventID: MAP_EVENT_ID[row.event], //xd 16
       players: [
         {
           officialName: row.player1.trim(),
-          // club: row.team.trim(),
-          birthDate: moment(`1-1-${Number(row.player1Birth) - 543}`, "MM-DD-YYYY"),
-          gender: mapGenderDetail[row.player1Gender].gender,
+          club: row.team1.trim(),
+          birthDate: isNaN(Number(row.player1Birth)) ? undefined : moment(`1-1-${Number(2022 - Number(row.player1Birth))}`, "MM-DD-YYYY"),
+          // birthDate: moment(`1-1-${Number(row.player1Birth) - 543}`, "MM-DD-YYYY"),
+          // gender: mapGenderDetail[row.player1Gender].gender,
           displayName: row.player1Nick
         },
         {
           officialName: row.player2.trim(),
-          // club: row.team.trim(),
-          birthDate: moment(`1-1-${Number(row.player2Birth) - 543}`, "MM-DD-YYYY"),
-          gender: mapGenderDetail[row.player2Gender].gender,
+          club: row.team1.trim(),
+          birthDate: isNaN(Number(row.player2Birth)) ? undefined : moment(`1-1-${Number(2022 - Number(row.player2Birth))}`, "MM-DD-YYYY"),
+          // birthDate: moment(`1-1-${Number(row.player2Birth) - 543}`, "MM-DD-YYYY"),
+          // gender: mapGenderDetail[row.player2Gender].gender,
           displayName: row.player2Nick
         }
       ],
@@ -75,7 +64,7 @@ const script = async () => {
 
 
   })
+  console.log('done')
   return
 }
-
 script()
